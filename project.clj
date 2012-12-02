9f1d5e766956098f2e6117283eb9a8198e1bc025
David Nolen <dnolen@Davids-MacBook-Pro-2.local>
2012-12-02 16:46:23 -0500

    add index.html, fix deps, add repl ns, delete core.clj

--- project.clj
+++ project.clj
@@ -1,14 +1,18 @@
 (defproject techmesh-cljs "0.1.0-SNAPSHOT"
-  :description "TechMesh Demo of ClojureScript"
+  :description "TechMesh Conference Demo of ClojureScript"
 
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.match "0.2.0-alpha11"]
-                 [org.clojure/core.logic "0.8.0-beta3"]]
+                 [org.clojure/core.match "0.2.0-alpha10"]
+                 [org.clojure/tools.macro "0.1.1"]
+                 [org.clojure/core.logic "0.8.0-beta2"]]
 
   :plugins [[lein-cljsbuild "0.2.9"]]
-
   
-  :cljsbuild {:builds {:zebra {:source-path "src/techmesh-cljs/zebra"
+  :cljsbuild {:builds {:repl  {:source-path "src/techmesh-cljs/repl"
+                               :compiler {:optimizations :advanced
+                                          :static-fns true
+                                          :output-to "repl.js"}}
+                       :zebra {:source-path "src/techmesh-cljs/zebra"
                                :compiler {:optimizations :advanced
                                           :static-fns true
                                           :output-to "zebra.js"}}
---
+++ src/techmesh_cljs/repl/core.cljs
@@ -0,0 +1,3 @@
+(ns techmesh-cljs.repl.core
+  )
+
